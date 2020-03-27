12262806c33f4e778402d6c5442ed72d1ac22257
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-03-28 00:02:00 +0530

    Removed Token from header

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -5,8 +5,7 @@ load_dotenv()
 
 def get_user_id(token):
     try:
-        token = token.split(" ")
-        payload = jwt.decode(token[1], os.getenv('JWT_SECRET'), algorithms=['HS256'])
+        payload = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=['HS256'])
         payload['message'] = "Success"
         return payload
     except Exception as error:
