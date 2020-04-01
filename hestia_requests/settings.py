279913fb6cf96c0d207b3470196251b1fc6ee28f
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-04-01 14:34:40 +0530

    Corrected description

--- hestia_requests/settings.py
+++ hestia_requests/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os.path import sys
 from dotenv import load_dotenv
 import django_heroku
+
 load_dotenv()
+
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,5 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 django_heroku.settings(locals())
